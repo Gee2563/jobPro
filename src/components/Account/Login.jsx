7241e6ad1ce5bd01d640003224e38aa8b119bffd
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-30 11:56:29 +0100

    refactor: Remove unused import in Login component

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import '../../style.css';
+
 
 function Login() {
   const [email, setEmail] = useState('');
