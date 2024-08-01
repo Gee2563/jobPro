80fa8891966702afc53b216a27fd19324268a067
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-01 14:54:03 +0100

    feat: Add GenLetter and ViewGenLetter components for cover letter functionality

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import AllUploadedCv from './components/UploadedCvs/AllUploadedCv';
 import UploadACv from './components/UploadedCvs/UploadACv';
 import ViewUploadedCv from './components/UploadedCvs/ViewUploadedCv';
 import { AuthProvider } from './context/AuthContext';
+import GenLetter from "./components/CoverLetters/GenLetter";
+import ViewGenLetter from './components/CoverLetters/ViewGenLetter';
 
 
 // Issue with reloading - When I hit refresh, I'm automatically sent to login despite being logged in.
@@ -40,14 +42,19 @@ function App() {
             <Route path="/applications" element={<ProtectedRoute><AllApplications /></ProtectedRoute>} />
             <Route path="/applications/new" element={<ProtectedRoute><NewApplication /></ProtectedRoute>} />
             <Route path="/applications/:id" element={<ProtectedRoute><ViewApplication /></ProtectedRoute>} />
+            
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/projects" element={<Projects />} />
+            
             <Route path="/tailoredcv" element={<ProtectedRoute><TailorCv /></ProtectedRoute>} />
             <Route path="/tailoredcv/:id" element={<ProtectedRoute><ViewTailoredCv /></ProtectedRoute>} />
             <Route path="/uploadedcv" element={<ProtectedRoute><AllUploadedCv /></ProtectedRoute>} />
             <Route path="/uploadedcv/new" element={<ProtectedRoute><UploadACv /></ProtectedRoute>} />
             <Route path="/uploadedcv/:id" elemgent={<ProtectedRoute><ViewUploadedCv /></ProtectedRoute>} />
+            <Route path="/genletter" element={<ProtectedRoute><GenLetter /></ProtectedRoute>} />
+            <Route path="/genletter/:id" element={<ProtectedRoute><ViewGenLetter /></ProtectedRoute>} />
+            
           </Routes>
         <Footer />
       </Router>
