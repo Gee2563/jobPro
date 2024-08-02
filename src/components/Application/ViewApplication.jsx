cc3bb98d730c0c92c7ae930f2a67d959e5bc7cc8
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-02 10:13:10 +0100

    refactor: changed label layout and add divs for styling

--- src/components/Application/ViewApplication.jsx
+++ src/components/Application/ViewApplication.jsx
@@ -1,29 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getApplicationById, updateApplication } from '../../services/applicationApi';
+import React, { useState } from 'react';
+import { updateApplication } from '../../services/applicationApi'; // Import the function
 
-
-function ViewApplication() {
-  const { id } = useParams();
-  const [application, setApplication] = useState(null);
-  const [isEditing, setIsEditing] = useState(false); // Track if the form is in edit mode
-
-  useEffect(() => {
-    const fetchApplication = async () => {
-      try {
-        const response = await getApplicationById(id);
-        console.log('Application:', response);
-        setApplication(response);
-      } catch (error) {
-        console.error('Failed to fetch application', error);
-      }
-    };
-    fetchApplication();
-  }, [id]);
-
-  if (!application) {
-    return <div>Loading...</div>;
-  }
+const ViewApplication = ({ application, onClose }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedApplication, setUpdatedApplication] = useState(application);
 
   const handleEdit = () => {
     setIsEditing(true);
@@ -31,148 +11,162 @@ function ViewApplication() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setApplication((prevApp) => ({ ...prevApp, [name]: value }));
+    setUpdatedApplication((prevApp) => ({ ...prevApp, [name]: value }));
   };
 
   const handleUpdate = async () => {
     try {
-      await updateApplication(id, application);
+      await updateApplication(updatedApplication._id, updatedApplication);
       alert('Application updated');
-      setIsEditing(false); // Exit edit mode after successful update
+      setIsEditing(false);
     } catch (error) {
       console.error('Failed to update application', error);
     }
   };
 
-  // Can this be refactored?
   return (
-    <div>
-      <h2>{application.companyName}</h2>
-      <img src={`https://img.logo.dev/${application.companyWebsite}?token=pk_GS8EES80RXOLepVgd1-2ZQ`} alt="Company Logo" />
-      <div>
-        <label>Company Website</label>
-        {isEditing ? (
-          <input
-            type="text"
-            name="companyWebsite"
-            value={application.companyWebsite}
-            onChange={handleChange}
-          />
-        ) : (
-          <a href={application.companyWebsite} target="_blank" rel="noreferrer">
-            {application.companyWebsite}
-          </a>
-        )}
-      </div>
-      <div>
-        <label>Job Title:</label>
-        {isEditing ? (
-          <input
-            type="text"
-            name="jobTitle"
-            value={application.jobTitle}
-            onChange={handleChange}
-          />
-        ) : (
-          <span>{application.jobTitle}</span>
-        )}
-      </div>
-      <div>
-        <label>Pay:</label>
-        {isEditing ? (
-          <input
-            type="text"
-            name="pay"
-            value={application.pay}
-            onChange={handleChange}
-          />
-        ) : (
-          <span>{application.pay}</span>
-        )}
-      </div>
-      <div>
-        <label>Job Description:</label>
-        {isEditing ? (
-          <textarea
-            name="jobDescription"
-            value={application.jobDescription}
-            onChange={handleChange}
-          />
-        ) : (
-          <span>{application.jobDescription}</span>
-        )}
-      </div>
-      <div>
-        <label>Remote/Hybrid:</label>
-        {isEditing ? (
-          <input
-            type="text"
-            name="remoteHybrid"
-            value={application.remoteHybrid}
-            onChange={handleChange}
-          />
-        ) : (
-          <span>{application.remoteHybrid}</span>
-        )}
-      </div>
-      <div>
-        <label>Location:</label>
-        {isEditing ? (
-          <input
-            type="text"
-            name="location"
-            value={application.location}
-            onChange={handleChange}
-          />
-        ) : (
-          <span>{application.location}</span>
-        )}
-      </div>
-      <div>
-        <label>Comments:</label>
-        {isEditing ? (
-          <textarea
-            name="comments"
-            value={application.comments}
-            onChange={handleChange}
-          />
-        ) : (
-          <span>{application.comments}</span>
-        )}
-      </div>
-      <div>
-        <label>Urgency Score:</label>
-        <span>{application.urgencyScore}</span>
-      </div>
-      <div>
-        <label>Stage:</label>
-        {isEditing ? (
-          <select
-            name="stage"
-            value={application.stage}
-            onChange={handleChange}
-          >
-            <option value="active/research">Active/Researching</option>
-            <option value="applied">Applied</option>
-        
-            <option value="interviewing">Interviewing</option>
-            <option value="offers">Offers</option>
-            <option value="reject/reviews">Reject/Reviews</option>
-          </select>
-        ) : (
-          <span>{application.stage}</span>
-        )}
-      </div>
-      {isEditing ? (
-        <div>
-          <button onClick={handleUpdate}>Update</button>
-          <button onClick={() => setIsEditing(false)}>Cancel</button>
+    <div className='view-application-container'>
+      <div className="view-application-card">
+        <div className="view-app-top-container">
+          <div className="view-app-top-left-side">
+            <img
+              src={`https://img.logo.dev/${application.companyWebsite}?token=pk_GS8EES80RXOLepVgd1-2ZQ`}
+              alt="Company Logo"
+            />
+          </div>
+          <div className="view-app-top-right-side">
+            <h2>{application.companyName}</h2>
+            <label>Company Website:
+              {isEditing ? (
+                <input
+                  type="text"
+                  name="companyWebsite"
+                  value={updatedApplication.companyWebsite}
+                  onChange={handleChange}
+                />
+              ) : (
+                <a href={application.companyWebsite} target="_blank" rel="noreferrer">
+                  {application.companyWebsite}
+                </a>
+              )}
+            </label>
+            <label>Job Title:
+              {isEditing ? (
+                <input
+                  type="text"
+                  name="jobTitle"
+                  value={updatedApplication.jobTitle}
+                  onChange={handleChange}
+                />
+              ) : (
+                <span>{application.jobTitle}</span>
+              )}
+            </label>
+            <label>Pay:
+              {isEditing ? (
+                <input
+                  type="text"
+                  name="pay"
+                  value={updatedApplication.pay}
+                  onChange={handleChange}
+                />
+              ) : (
+                <span>{application.pay}</span>
+              )}
+            </label>
+          </div>
         </div>
-      ) : (
-        <button onClick={handleEdit}>Edit</button>
-      )}
+        <div className="view-app-bottom-container">
+          <label>Job Description:
+            {isEditing ? (
+              <textarea
+                name="jobDescription"
+                value={updatedApplication.jobDescription}
+                onChange={handleChange}
+              />
+            ) : (
+              <span>{application.jobDescription}</span>
+            )}
+          </label>
+          <label>Remote/Hybrid:
+            {isEditing ? (
+              <input
+                type="text"
+                name="remoteHybrid"
+                value={updatedApplication.remoteHybrid}
+                onChange={handleChange}
+              />
+            ) : (
+              <span>{application.remoteHybrid}</span>
+            )}
+          </label>
+          <label>Location:
+            {isEditing ? (
+              <input
+                type="text"
+                name="location"
+                value={updatedApplication.location}
+                onChange={handleChange}
+              />
+            ) : (
+              <span>{application.location}</span>
+            )}
+          </label>
+          <label>Comments:
+            {isEditing ? (
+              <textarea
+                name="comments"
+                value={updatedApplication.comments}
+                onChange={handleChange}
+              />
+            ) : (
+              <span>{application.comments}</span>
+            )}
+          </label>
+          <label>Application Date:
+            {isEditing ? (
+              <input
+                type="date"
+                name="applicationDate"
+                value={updatedApplication.applicationDate}
+                onChange={handleChange}
+              />
+            ) : (
+              <span>{application.applicationDate}</span>
+            )}
+          </label>
+          <label>Stage:
+            {isEditing ? (
+              <select
+                name="stage"
+                value={updatedApplication.stage}
+                onChange={handleChange}
+              >
+                <option value="active/research">Active/Researching</option>
+                <option value="applied">Applied</option>
+                <option value="interviewing">Interviewing</option>
+                <option value="offers">Offers</option>
+                <option value="reject/reviews">Reject/Reviews</option>
+              </select>
+            ) : (
+              <span>{application.stage}</span>
+            )}
+          </label>
+        </div>
+        <div>
+          {isEditing ? (
+            <div>
+              <button onClick={handleUpdate}>Update</button>
+              <button onClick={() => setIsEditing(false)}>Cancel</button>
+            </div>
+          ) : (
+            <button onClick={handleEdit}>Edit</button>
+          )}
+        </div>
+      
+      </div>
     </div>
   );
-}
+};
 
 export default ViewApplication;
